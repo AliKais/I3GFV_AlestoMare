054c2cd051360b5b7f8cff3bd3aa7c61dd596c03
Kaison <alikais98@gmail.com>
2019-05-08 23:26:11 +0200

    Measure fixet

--- Aflevering 4/Scale.cydsn/measure.c
+++ Aflevering 4/Scale.cydsn/measure.c
@@ -10,9 +10,7 @@ void set(int x){
 }
 
 void uartADCinit(){
-    
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-       
+           
     UART_1_Start();
     ADC_SAR_1_Start();
     ADC_SAR_1_StartConvert();
@@ -23,27 +21,17 @@ void measure(){
     
     uint16_t result = ADC_SAR_1_GetResult16();
 
-    float stigning = 4.641142;    // Stigning
-    int offset = -288;          // Standard værdi når vægten er belastet med 1000g
-    int weight = stigning * (result + offset);    // Ligning for ret linje y=ax+b
+    float stigning = 0.715;                         // Stigning
+    int offset = -1612;                             // Offset
+    float weight = stigning * (result + offset);    // Ligning for ret linje
     
-    if (measureBool == 1)
-    {
-        //Kode til udførelse at reset
-        measureValue = weight;
-        measureBool = 0;
-        UART_1_PutString("Setting tare value");
-    }    
 
-    //vægt minus measureValue
-    weight = weight - measureValue;
-
-    //decimal værdi
+    //Decimal værdi
     snprintf(uartBuffer, sizeof(uartBuffer), "ADC result: %d \r\n", result);
     UART_1_PutString(uartBuffer);
 
-    //vægten
-    snprintf(uartBuffer, sizeof(uartBuffer), "weight Result: %d gram \r\n\r\n", weight);
+    //Vægten
+    snprintf(uartBuffer, sizeof(uartBuffer), "Omregnet vaegt: %f gram \r\n\r\n", weight);
     UART_1_PutString(uartBuffer);
     
 }
