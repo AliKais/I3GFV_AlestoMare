f91843a17f3628b892f725a8721b5606602c433a
Saka <m.m.hagler@outlook.dk>
2019-05-08 16:39:44 +0200

    Create measure.c
    dad

---
+++ Aflevering 4/measure.c
@@ -0,0 +1,49 @@
+#include "measure.h"
+
+char uartBuffer[256];
+int measureBool = 0;
+int measureValue = 0;
+
+
+void set(int x){
+    measureBool = x;
+}
+
+void uartADCinit(){
+    
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+       
+    UART_1_Start();
+    ADC_SAR_1_Start();
+    ADC_SAR_1_StartConvert();
+    
+}
+
+void measure(){
+    
+    uint16_t result = ADC_SAR_1_GetResult16();
+
+    float stigning = 0.715; // Stigning
+    int offset = -1612;          // Standard værdi når vægten er belastet med 1000g
+    float weight = stigning * (result + offset);    // Ligning for ret linje y=ax+b
+    
+//    if (measureBool == 1)
+//    {
+//        //Kode til udførelse at reset
+//        measureValue = weight;
+//        measureBool = 0;
+//        UART_1_PutString("Setting tare value");
+//    }    
+//
+//    //vægt minus measureValue
+//    weight = weight - measureValue;
+
+    //decimal værdi
+    snprintf(uartBuffer, sizeof(uartBuffer), "ADC result: %d \r\n", result);
+    UART_1_PutString(uartBuffer);
+
+    //vægten
+    snprintf(uartBuffer, sizeof(uartBuffer), "Omregnet vaegt: %f gram \r\n\r\n", weight);
+    UART_1_PutString(uartBuffer);
+    
+}
